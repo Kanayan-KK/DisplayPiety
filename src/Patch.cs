66c2cb3df74d2d2063a032a5f5686acd4b346ce2
Kanayan <glgln6@gmail.com>
2025-12-10 22:16:00 +0900

    v1.0.0

--- src/Patch.cs
+++ src/Patch.cs
@@ -5,12 +5,16 @@ namespace DisplayPiety
     [HarmonyPatch]
     public static class Patch
     {
+        private const int CHARM_ID = 77;
+        private const int PIETY_ID = 85;
+        private const int FAITH_ID = 306;
+
         [HarmonyPostfix]
         [HarmonyPatch(typeof(WidgetStatsBar), nameof(WidgetStatsBar.Add))]
         public static void WidgetStatsBar_Add_Postfix(WidgetStatsBar __instance, Element ele)
         {
             // ウィジェットバーの魅力ステータスアイコンの後に追加
-            if(ele is not (Element)null && ele?.id == 77)
+            if (ele is not (Element)null && ele?.id == CHARM_ID)
                 AddPietyWidget(__instance);
         }
 
@@ -21,11 +25,11 @@ namespace DisplayPiety
                 if (EClass.pc == null)
                     return "-/-";
 
-                // 信仰心スキル値
-                var piety = EClass.pc.Evalue(85);
+                // 信仰心ステータス値
+                var piety = EClass.pc.Evalue(PIETY_ID);
 
-                // 信仰スキル値
-                var faith = EClass.pc.Evalue(306);
+                // 信仰ステータス値
+                var faith = EClass.pc.Evalue(FAITH_ID);
 
                 return $"{piety} / {faith}";
             });
